a4050090c4f540d70c0e1cb9e301ec26abb234ce
Jordan Delbar <jordan.delbar@adeo.com>
2023-08-20 19:39:51 +0200

    test: Add health_check integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
-
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_ckeck)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
-}
 
 async fn health_ckeck() -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_ckeck)))
+        .bind("127.0.0.1:8000")?
+        .run();
+    Ok(server)
 }
