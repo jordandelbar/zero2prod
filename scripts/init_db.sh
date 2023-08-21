f1b01a6e10796c51264e183c9c5734856e6d9805
Jordan Delbar <jordan.delbar@adeo.com>
2023-08-21 11:32:03 +0200

    feat: Add first migration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,16 +2,6 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "${command -v psql}" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
-fi
-
-if ! [ -x "${command -v sqlx}" ]; then
-  echo >&2 "Error: sqlx is not installed."
-  exit 1
-fi
-
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
 # Check if a custom password has been set, otherwise default to 'password'
@@ -23,15 +13,19 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-# Launch postgres using Docker
-docker run \
-  -e POSTGRES_USER=${DB_USER} \
-  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
-  -d postgres \
-  postgres -N 1000
-  # ^ Increased maximum number of connections for testing purposes
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  # Launch postgres using Docker
+  docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+    # ^ Increased maximum number of connections for testing purposes
+fi
 
 # Keep pinging Postgres until it's ready to accept comands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -44,5 +38,8 @@ done
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
-sqlx database create:w
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
 
